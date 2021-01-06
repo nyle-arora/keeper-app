9eb704e4c91d5c15f31d55a8d52b57912bb86af8
Nyle Arora <nylearora2021@u.northwestern.edu>
2021-01-05 18:20:40 -0600

    section 1 done

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import notes from "../notes";
+
+// function App() {
+//   return (
+//     <div>
+//     <h1>Successful initialization of your project! Delete this message and get started. May the force be with you!</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+            <h1>Keeper App</h1>
+        </header>
+    );
+}
+
+export default Header;
